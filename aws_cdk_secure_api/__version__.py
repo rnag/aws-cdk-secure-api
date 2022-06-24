3ad759a1c82a9174edb7311eca7e24fd751c353c
Ritvik Nag <rnag@collegeboard.org>
2022-06-24 15:08:01 -0400

    Bump version: 0.1.1 → 0.1.2

--- aws_cdk_secure_api/__version__.py
+++ aws_cdk_secure_api/__version__.py
@@ -5,7 +5,7 @@ AWS CDK Secure API - A CDK (v2) Construct Library for Secure REST APIs
 __title__ = 'aws-cdk-secure-api'
 __description__ = 'A CDK (v2) Construct Library for Secure REST APIs'
 __url__ = 'https://github.com/rnag/aws-cdk-secure-api'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'Ritvik Nag'
 __author_email__ = 'rv.kvetch@gmail.com'
 __license__ = 'MIT'
